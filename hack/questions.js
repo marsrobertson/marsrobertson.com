963c951b4021af335266fc8268510ea91a46a787
Mars Notahacker <marsXrobertson@gmail.com>
2024-10-27 10:43:25 +0000

    cleanup / restructure

---
+++ hack/questions.js
@@ -0,0 +1,23 @@
+let questions = [
+    {
+        id: 1,
+        optionA: "Pepsi",
+        imageA: "./images/pepsi.png",
+        optionB: "Coke",
+        imageB: "./images/coke.png",
+    },
+    {
+        id: 2,
+        optionA: "Kamala",
+        imageA: "./images/kamala.png",
+        optionB: "Trump",
+        imageB: "./images/trump.png",
+    },
+    {
+        id: 3,
+        optionA: "She",
+        imageA: "./images/she.png",
+        optionB: "He",
+        imageB: "./images/he.png",
+    },
+]
